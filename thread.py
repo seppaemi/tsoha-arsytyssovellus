87ca814cb592d096b61aea2ab26755476a9b9c36
seppaemi <emilia.seppala@helsinki.fi>
2022-09-21 12:43:08 +0300

    works

--- thread.py
+++ thread.py
@@ -1,5 +1,5 @@
 from db import db
-from counter import decrement_tc, increment_tc, sub
+#from counter import decrement_tc, increment_tc, sub
 
 def create(title, forum_id, user_id):
     if title.strip():
